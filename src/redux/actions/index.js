331d6e03d78640de1346796c4e52df4998f5ae55
Levy Bezerra <levybholanda@gmail.com>
2022-08-26 15:11:44 -0300

    gerenciando Redux

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,13 @@
 // Coloque aqui suas actions
+export const USER_INFO = 'USER_INFO';
+export const WALLET_INFO = 'WALLET_INFO';
+
+export const userData = (user) => ({
+  type: USER_INFO,
+  payload: user,
+});
+
+export const walletData = (wallet) => ({
+  type: WALLET_INFO,
+  payload: wallet,
+});
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,25 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+
+import { WALLET_INFO } from '../actions';
+
+const INITIAL_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+    editor: false,
+    idToEdit: 0,
+  },
+};
+
+const walletReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case WALLET_INFO:
+    return {
+      type: WALLET_INFO,
+      wallet: { ...action.payload },
+    };
+  default: return state;
+  }
+};
+
+export default walletReducer;
